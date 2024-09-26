6ee8dfb530e2b9d82c98012797d004ef13899886
otto <ottobrunok@gmail.com>
2024-09-26 13:53:58 +0300

    ylesanded

---
+++ yl1.2.js
@@ -0,0 +1,5 @@
+const aasta = 2016;
+let president = "Kersti Kaljulaid";
+let lauseKeskosa = ". aastal valiti Eesti presidendiks";
+const lause = aasta + president + lauseKeskosa;
+console.log(lause);
---
+++ yl1.3.js
@@ -0,0 +1,9 @@
+let alus = 2
+let astendaja = 4
+console.log(alus**astendaja);
+
+    
+    
+    
+
+
